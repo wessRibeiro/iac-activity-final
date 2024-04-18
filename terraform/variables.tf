1e1553c7f22fba35c3a072f9871b93528213c30f
Weslley Ribeiro <wess_ribeiro@hotmail.com>
2024-04-17 22:13:57 -0300

    arrumando vars, criando tfvars

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,24 @@
 
 variable "aws_region" {
-
+    default = "us-east-1"
 }
 
 variable "aws_security_group_name" {
-
-}
-
-
-variable "terraform_provider_required_version" {
-
-}
-
-variable "terraform_provider_version" {
-
+    default = "grupo do wess"
 }
 
 variable "ami" {
-
+    default = "ami-0fc5d935ebf8bc3bc"
 }
 
 variable "instance_type" {
-
+    default = "t2.micro"
 }
 
 variable "key_name" {
-
+    default = "ssh-key-wess"
 }
 
 variable "public_key" {
-
+    default = ""
 }
